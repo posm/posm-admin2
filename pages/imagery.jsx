5a115e2aa16909ffe4c08b3b823977d50f86f513
Seth Fitzsimmons <seth@mojodna.net>
2016-10-27 18:07:40 -0700

    Remove naming option when uploading new imagery
    Since it can now be renamed after the fact instead.

--- pages/imagery.jsx
+++ pages/imagery.jsx
@@ -22,12 +22,9 @@ export default class Index extends React.Component {
     super(props);
 
     this.getSources = this.getSources.bind(this);
-    this.saveSource = this.saveSource.bind(this);
-    this.updateName = this.updateName.bind(this);
   }
 
   state = {
-    name: "",
     sources: {},
   }
 
@@ -35,7 +32,7 @@ export default class Index extends React.Component {
     this.getSources();
   }
 
-  getSources() {
+  getSources(callback = () => {}) {
     const { endpoint } = this.props;
 
     fetch(`${endpoint}/imagery`)
@@ -44,30 +41,10 @@ export default class Index extends React.Component {
         this.setState({
           sources
         });
+
+        callback(sources);
       })
       .catch(err => console.warn(err.stack));
-  }
-
-  saveSource(url) {
-    const { name } = this.state;
-
-    // reset name
-    this.setState({
-      name: "",
-    });
-
-    if (name !== "") {
-      // update metadata
-      fetch(url, {
-        body: JSON.stringify({
-          name,
-        }),
-        method: "PATCH"
-      }).then(this.getSources)
-        .catch(err => console.warn(err.stack));
-    } else {
-      this.getSources();
-    }
   }
 
   updateName(event) {
@@ -79,7 +56,6 @@ export default class Index extends React.Component {
   render() {
     const { endpoint } = this.props;
     const { sources } = this.state;
-    const sourceName = this.state.name;
 
     const imagery = Object.keys(sources)
       .filter(name => sources[name] !== {})
@@ -108,40 +84,27 @@ export default class Index extends React.Component {
 
         <div className="row">
           <div className="x_panel">
-            <div className="x_title">
-              <h2>New Source</h2>
-              <div className="clearfix" />
-            </div>
-
             <div className="x_content">
-              <form className="form-horizontal form-label-left">
-                <div className="form-group">
-                  <input type="text" className="form-control" placeholder="Source Name" value={sourceName} onChange={this.updateName} />
-                </div>
-                <Dropzone
-                  config={{
-                    postUrl: "no-url",
-                  }}
-                  eventHandlers={{
-                    init: dropzone => {
-                      this.dropzone = dropzone;
-                    },
-                    success: (file, rsp) => {
-                      this.saveSource(rsp.source);
-
+              <Dropzone
+                config={{
+                  postUrl: `${endpoint}/imagery/upload`,
+                }}
+                eventHandlers={{
+                  init: dropzone => {
+                    this.dropzone = dropzone;
+                  },
+                  success: (file, rsp) => {
+                    this.getSources(() => {
                       this.dropzone.removeFile(file);
-                    },
-                    processing: () => {
-                      this.dropzone.options.url = `${endpoint}/imagery/upload`;
-                    },
-                  }}
-                  djsConfig={{
-                    acceptedFiles: "image/tiff",
-                    addRemoveLinks: false,
-                    method: "PUT",
-                  }}
-                />
-              </form>
+                    });
+                  },
+                }}
+                djsConfig={{
+                  acceptedFiles: "image/tiff",
+                  addRemoveLinks: false,
+                  method: "PUT",
+                }}
+              />
             </div>
           </div>
         </div>
